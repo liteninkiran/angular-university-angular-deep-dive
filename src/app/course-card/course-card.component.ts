bc18ec390e8ee8d474d81a3260dae1441b16f52f
Kiran Anand <liteninkiran@hotmail.com>
2025-08-20 13:07:08 +0100

    008 - Building our First Angular Component

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'course-card',
+    imports: [],
+    templateUrl: './course-card.component.html',
+    styleUrl: './course-card.component.css',
+})
+export class CourseCardComponent {}
