7530b8dbb4ac1bf34e5f06040ecf3821ccc3873e
Kiran Anand <liteninkiran@hotmail.com>
2025-08-24 16:22:41 +0100

    035 - Angular Content Child Decorator - In-Depth Explanation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, CourseCardComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        CourseCardComponent,
+        CourseImageComponent,
+        CourseImageComponent,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,6 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    ContentChild,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+} from '@angular/core';
 import { Course } from '../model/course';
 import { CommonModule } from '@angular/common';
+import { CourseImageComponent } from '../course-image/course-image.component';
 
 @Component({
     selector: 'course-card',
@@ -8,7 +17,7 @@ import { CommonModule } from '@angular/common';
     templateUrl: './course-card.component.html',
     styleUrl: './course-card.component.css',
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements AfterViewInit {
     @Input({ required: true })
     public course: Course;
 
@@ -17,6 +26,13 @@ export class CourseCardComponent {
 
     @Output('courseSelected')
     public courseEmitter = new EventEmitter<Course>();
+
+    @ContentChild(CourseImageComponent, { read: ElementRef })
+    public image: ElementRef;
+
+    public ngAfterViewInit(): void {
+        console.log(this.image);
+    }
 
     public onCourseView(): void {
         this.courseEmitter.emit(this.course);
