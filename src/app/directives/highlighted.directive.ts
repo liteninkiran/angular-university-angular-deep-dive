a9b478608b907440e09eb661e07fe8f3a656bb98
Kiran Anand <liteninkiran@hotmail.com>
2025-08-25 11:52:22 +0100

    041 - Angular Host Binding in Detail - DOM Properties vs Attributes

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
     selector: '[highlighted]',
 })
 export class HighlightedDirective {
-    constructor() {
-        console.log('Directive Created');
+    @Input({
+        alias: 'highlighted',
+        transform: (value: unknown) => value === true,
+    })
+    public isHighlighted = false;
+
+    constructor() {}
+
+    // @HostBinding('className')
+    // get cssClasses() {
+    //     return 'highlighted';
+    // }
+
+    // @HostBinding('style.border')
+    // get cssStyles() {
+    //     return '1px solid red';
+    // }
+
+    @HostBinding('class.highlighted')
+    get cssClasses() {
+        return this.isHighlighted;
+    }
+
+    @HostBinding('attr.disabled')
+    get disabled() {
+        return true;
     }
 }
