15f66be25b91269d7c9b5f5c8195a587943585a0
Kiran Anand <liteninkiran@hotmail.com>
2025-08-26 22:06:53 +0100

    055 - Angular Custom Service - Fetching Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { COURSES } from '../db-data';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Course } from './model/course';
 import { Observable } from 'rxjs';
 import { CoursesService } from './services/courses.service';
@@ -14,13 +12,9 @@ import { CoursesService } from './services/courses.service';
 export class AppComponent implements OnInit {
     public courses$: Observable<Course[]>;
 
-    constructor(
-        private http: HttpClient,
-        private coursesService: CoursesService,
-    ) {}
+    constructor(private coursesService: CoursesService) {}
 
     public ngOnInit(): void {
-        const params = new HttpParams().set('page', 1).set('pageSize', 3);
-        this.courses$ = this.http.get<Course[]>('/api/courses', { params });
+        this.courses$ = this.coursesService.loadCourses();
     }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CoursesService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
+
+    public loadCourses(): Observable<Course[]> {
+        const params = new HttpParams().set('page', 1).set('pageSize', 3);
+        return this.http.get<Course[]>('/api/courses', { params });
+    }
 }
