fdec292485e68848433b1b145f11884b315366e4
Kiran Anand <liteninkiran@hotmail.com>
2025-09-01 21:24:12 +0100

    089 - Angular 17 @defer Section Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,30 @@
-import {
-    ChangeDetectorRef,
-    Component,
-    Inject,
-    Injector,
-    OnInit,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Course } from './model/course';
 import { CoursesService } from './courses/courses.service';
-import { AppConfig, CONFIG_TOKEN } from './config';
-import { COURSES } from 'src/db-data';
-import { createCustomElement, NgElementConfig } from '@angular/elements';
-import { CourseTitleComponent } from './course-title/course-title.component';
-import { NgForOf } from '@angular/common';
 import { CourseCardComponent } from './courses/course-card/course-card.component';
 import { CourseImageComponent } from './courses/course-image/course-image.component';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [NgForOf, CourseCardComponent, CourseImageComponent],
+    imports: [CourseCardComponent, CourseImageComponent, CommonModule],
 })
 export class AppComponent implements OnInit {
-    public courses = COURSES;
-    public numCourses = COURSES.length;
+    public courses$: Observable<Course[]>;
 
-    constructor(
-        private coursesService: CoursesService,
-        private cd: ChangeDetectorRef,
-        @Inject(CONFIG_TOKEN) private config: AppConfig,
-        private injector: Injector,
-    ) {}
+    constructor(private coursesService: CoursesService) {}
 
     public ngOnInit(): void {
-        const config: NgElementConfig = {
-            injector: this.injector,
-        };
-        const htmlElement = createCustomElement(CourseTitleComponent, config);
-        customElements.define('course-title', htmlElement);
+        this.courses$ = this.coursesService.loadCourses();
     }
 
     public saveCourse(course: Course): void {
         this.coursesService
             .saveCourse(course)
             .subscribe((data) => console.log(data));
-    }
-
-    public onEditCourse() {
-        this.courses[0].category = 'ADVANCED';
     }
 }
