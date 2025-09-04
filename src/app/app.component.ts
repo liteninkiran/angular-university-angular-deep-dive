4f67fba1cf110fbbffce0950a12bfe56cfc2979b
Kiran Anand <liteninkiran@hotmail.com>
2025-09-04 14:39:56 +0100

    103 - Angular Signals with Objects and Arrays - Avoid mutating signal values directly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,26 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
     public counter = signal(0);
+    public course = signal({
+        id: 1,
+        title: 'Angular For Beginners',
+    });
+    public courses = signal([
+        'Angular For Beginners',
+        'Reactive Angular',
+        'Signals Deep Dive',
+    ]);
 
-    constructor() {
-        // const readOnlySignal = this.counter.asReadonly();
-    }
+    constructor() {}
 
     public ngOnInit(): void {}
 
     public increment(): void {
         this.counter.update((val) => val + 1);
+        this.course.set({
+            id: 1,
+            title: 'Hello World',
+        });
+        this.courses.update((courses) => [...courses, 'New Course']);
     }
 }
